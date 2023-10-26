a8425f069d71f10922854a2d7d9b02fe3d378349
Nathalie Castells-Brooke <36696807+nathcast@users.noreply.github.com>
2023-10-26 17:11:33 +0100

    Base: social stream and livewire - working

---
+++ config/socialstream.php
@@ -0,0 +1,20 @@
+<?php
+
+use JoelButcher\Socialstream\Features;
+use JoelButcher\Socialstream\Providers;
+
+return [
+    'middleware' => ['web'],
+    'prompt' => 'Or Login Via',
+    'providers' => [
+        Providers::github(),
+        Providers::google(),
+    ],
+    'features' => [
+        Features::createAccountOnFirstLogin(),
+        // Features::generateMissingEmails(),
+        Features::rememberSession(),
+        Features::providerAvatars(),
+        Features::refreshOAuthTokens(),
+    ],
+];
